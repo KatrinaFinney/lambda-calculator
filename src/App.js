76b831606440738cba300c018eecb64b11b072b5
Katrina Finney <katrinafinney@gmail.com>
2019-11-06 11:07:51 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display";
 
-function App() {
+function App(props) {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -16,9 +20,22 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <div className="display-container">
+      <Display />
+      </div>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-      </div>
+       <div>
+         <div className ="specials">
+         </div>
+        <Specials />  
+        
+        <div className ="operation-container">
+        <Numbers />
+        <Operators />
+        </div>
+        </div>
+    </div>
     </div>
   );
 }
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,22 @@
-import React from "react";
 
+import React, {useState} from "react";
+import {numbers} from "../../../data";
+import NumberButton from "./NumberButton"
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+const [numberButton, setNumberButton] = useState(numbers)
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className= "numbers" >
+      { numberButton.map((button, index) => (
+         <NumberButton key = {index} numbers = {button}/>
+      ))}
+     
     </div>
   );
 };
+
+export default Numbers
